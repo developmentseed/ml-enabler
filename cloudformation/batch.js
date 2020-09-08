0754d8a6e39fceeed080d83569d8feefcb06d643
ingalls <nick@ingalls.ca>
2020-09-08 08:56:40 -0700

    TFRecord job => Task.py

--- cloudformation/batch.js
+++ cloudformation/batch.js
@@ -229,7 +229,7 @@ const stack = {
                 },
                 Parameters: { },
                 ContainerProperties: {
-                    Command: ['./task.js'],
+                    Command: ['./task.py'],
                     Environment: [
                         { Name: 'StackName' , Value: cf.stackName },
                         { Name: 'BATCH_ECR' , Value: cf.ref('BatchECR') },
