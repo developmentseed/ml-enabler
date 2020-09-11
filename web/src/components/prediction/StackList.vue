28a2b09b04f3f272207b244dfb358aeb11e74548
ingalls <nick@ingalls.ca>
2020-09-11 10:40:33 -0700

    Add StackList submission

---
+++ web/src/components/prediction/StackList.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class='col col--12 grid grid--gut12'>
+        <label class='col col--10 pt12'>Submit "<span v-text='imagery.name'/>" For Inferencing</label>
+        <div class='col col--2 mb12'>
+            <button @click='$emit("queue")' class='fr btn btn--stroke round'>Submit</button>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'StackList',
+    props: ['imagery'],
+    data: function() {
+        return {};
+    },
+}
+</script>
