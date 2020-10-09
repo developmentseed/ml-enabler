c1406883205acd060ae8105d3318b2656832a8d7
martham93 <marthamorrissey93@gmail.com>
2020-10-09 11:26:08 -0600

    fix quotes

--- task-retrain/model_config.py
+++ task-retrain/model_config.py
@@ -15,21 +15,21 @@ class RetrainConfig():
 
 
     def __init__(self, payload: dict):
-        self.cycle_length = payload.get(cycle_length, 1)
-        self.n_map_threads = payload.get(n_map_threads, 4)
-        self.shuffle_buffer_size = payload.get(shuffle_buffer_size, 4)
-        self.prefetch_buffer_size = payload.get(prefetch_buffer_size, 1)
-        self.tf_steps_per_summary = payload.get(tf_steps_per_summary, 100)
-        self.tf_steps_per_checkpoint = payload.get(tf_steps_per_checkpoint, 200)
-        self.tf_batch_size = payload.get(tf_batch_size, 8)
-        self.tf_train_steps = payload.get(tf_train_steps, 800)
-        self.tf_dense_size_a = payload.get(tf_dense_size_a, 256)
-        self.tf_dense_dropout_rate_a = payload.get(tf_dense_dropout_rate_a, 0.34)
-        self.tf_dense_size = payload.get(tf_dense_size, 128)
-        self.tf_dense_dropout_rate = payload.get(tf_dense_dropout_rate, 0.35)
-        self.tf_dense_activation = payload.get(tf_dense_activation, 'relu')
-        self.tf_learning_rate = payload.get(tf_learning_rate, 0.00001)
-        self.tf_optimizer = payload.get(tf_optimizer, 'adam')
+        self.cycle_length = payload.get('cycle_length', 1)
+        self.n_map_threads = payload.get('n_map_threads', 4)
+        self.shuffle_buffer_size = payload.get('shuffle_buffer_size', 4)
+        self.prefetch_buffer_size = payload.get('prefetch_buffer_size', 1)
+        self.tf_steps_per_summary = payload.get('tf_steps_per_summary', 100)
+        self.tf_steps_per_checkpoint = payload.get('tf_steps_per_checkpoint', 200)
+        self.tf_batch_size = payload.get('tf_batch_size', 8)
+        self.tf_train_steps = payload.get('tf_train_steps', 800)
+        self.tf_dense_size_a = payload.get('tf_dense_size_a', 256)
+        self.tf_dense_dropout_rate_a = payload.get('tf_dense_dropout_rate_a', 0.34)
+        self.tf_dense_size = payload.get('tf_dense_size', 128)
+        self.tf_dense_dropout_rate = payload.get('tf_dense_dropout_rate', 0.35)
+        self.tf_dense_activation = payload.get('tf_dense_activation', 'relu')
+        self.tf_learning_rate = payload.get('tf_learning_rate', 0.00001)
+        self.tf_optimizer = payload.get('tf_optimizer', 'adam')
 
 
     def validate(self, payload:dict):
