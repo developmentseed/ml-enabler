26cfb727e319bef92395bd4bcc3acd4920094c25
ingalls <nick@ingalls.ca>
2020-10-21 12:26:06 -0400

    Show user type

--- ml_enabler/services/user_service.py
+++ ml_enabler/services/user_service.py
@@ -4,7 +4,12 @@ class UserService():
 
     @staticmethod
     def list(user_filter: str):
-        users = list(map(lambda user: { 'id': user.id, 'name': user.name }, User.list(user_filter)))
+        users = list(map(lambda user: {
+            'id': user.id,
+            'name': user.name,
+            'email': user.email,
+            'access': user.access
+        }, User.list(user_filter)))
 
         return {
             'users': users
