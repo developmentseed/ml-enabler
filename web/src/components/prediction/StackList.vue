7e6ecaa846e1c9c9edeeba612987d493429aa2ea
ingalls <nick@ingalls.ca>
2020-11-12 09:58:39 -0500

    Fix spacing of tilelist submission

--- web/src/components/prediction/StackList.vue
+++ web/src/components/prediction/StackList.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class='col col--12 grid grid--gut12'>
+    <div class='col col--12 grid grid--gut12 pt12'>
         <label class='col col--10 pt12'>Submit "<span v-text='imagery.name'/>" For Inferencing</label>
         <div class='col col--2 mb12'>
             <button @click='$emit("queue")' class='fr btn btn--stroke round'>Submit</button>
