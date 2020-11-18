b8fc89f5dfae1705a055e16b96d7fdc0156c043d
ingalls <nick@ingalls.ca>
2020-11-18 13:18:00 -0700

    Use streaming requests

--- lambda-pop/pop/handler.py
+++ lambda-pop/pop/handler.py
@@ -4,6 +4,7 @@ from io import StringIO
 import os, boto3, requests, csv, json
 from typing import Dict, Any
 from pop.custom_types import SQSEvent
+from contextlib import closing
 
 def handler(event: SQSEvent, context: Dict[str, Any]) -> bool:
     queue_name = event['queue']
@@ -13,32 +14,33 @@ def handler(event: SQSEvent, context: Dict[str, Any]) -> bool:
 
     queue = boto3.resource("sqs").get_queue_by_name(QueueName=queue_name)
 
-    r = requests.get(url)
-    r.raise_for_status()
+    with closing(requests.get(url, stream=True)) as r:
+        r.raise_for_status()
 
-    f = StringIO(r.text)
-    cache = []
-    first = True;
-    for row in csv.reader(f, delimiter=","):
-        if first == True:
-            first = False
-            continue
+        reader = csv.reader(r.iter_lines(), delimiter=',', quotechar='"')
 
-        cache.append({
-            "Id": row[0],
-            "MessageBody": json.dumps({
-                "name": row[0],
-                "url": row[1],
-                "bounds": list(map(lambda x: float(x), row[2].split(",")))
-            }),
-        })
+        cache = []
+        first = True;
+        for row in reader
+            if first == True:
+                first = False
+                continue
 
-        if len(cache) == 10:
+            cache.append({
+                "Id": row[0],
+                "MessageBody": json.dumps({
+                    "name": row[0],
+                    "url": row[1],
+                    "bounds": list(map(lambda x: float(x), row[2].split(",")))
+                }),
+            })
+
+            if len(cache) == 10:
+                queue.send_messages(Entries=cache)
+                cache = []
+
+        if len(cache) > 0:
             queue.send_messages(Entries=cache)
-            cache = []
-
-    if len(cache) > 0:
-        queue.send_messages(Entries=cache)
 
     return True
 
