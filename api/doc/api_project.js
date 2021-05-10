fc350df3c985c97bbdf9a6db2400c34dd846a6db
ingalls <nick@mapbox.com>
2021-05-10 09:54:21 -0600

    Successful docs

---
+++ api/doc/api_project.js
@@ -0,0 +1,15 @@
+define({
+  "name": "ML-Enabler",
+  "title": "ML-Enabler",
+  "version": "1.0.0",
+  "description": "",
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2021-05-10T15:54:09.448Z",
+    "url": "https://apidocjs.com",
+    "version": "0.27.1"
+  }
+});
