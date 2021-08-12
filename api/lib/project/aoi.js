46567ac9a36c4b7c1851cd75b2de91bea2226d14
ingalls <nick@ingalls.ca>
2021-08-12 09:49:16 -0600

    Add More AOI Schemas

---
+++ api/lib/project/aoi.js
@@ -0,0 +1,137 @@
+'use strict';
+
+const Err = require('../error');
+const { sql } = require('slonik');
+const Generic = require('../generic');
+
+/**
+ * @class
+ */
+class ProjectAOI extends Generic {
+    static _table = 'aois';
+
+    constructor() {
+        super();
+
+        this._table = ProjectAOI._table;
+
+        this.id = false;
+        this.created = false;
+        this.updated = false;
+        this.pid = false;
+        this.iter_id = false;
+        this.bounds = false;
+        this.name = false;
+
+        // Attributes which are allowed to be patched
+        this.attrs = Object.keys(require('../../schema/req.body.PatchAOI.json').properties);
+    }
+
+    /**
+     * Return a list of aois for a given project
+     *
+     * @param {Pool} pool - Instantiated Postgres Pool
+     *
+     * @param {Number} pid Project ID to access
+     *
+     * @param {Object} query - Query Object
+     * @param {Number} [query.limit=100] - Max number of results to return
+     * @param {Number} [query.page=0] - Page of users to return
+     * @param {String} [query.filter=] - Name to filter by
+     * @param {String} [query.sort=created] Field to sort by
+     * @param {String} [query.order=asc] Sort Order (asc/desc)
+     */
+    static async list(pool, pid, query) {
+        if (!query) query = {};
+        if (!query.limit) query.limit = 100;
+        if (!query.page) query.page = 0;
+        if (!query.filter) query.filter = '';
+
+        if (!query.sort) query.sort = 'created';
+        if (!query.order || query.order === 'asc') {
+            query.order = sql`asc`;
+        } else {
+            query.order = sql`desc`;
+        }
+
+        let pgres;
+        try {
+            pgres = await pool.query(sql`
+                SELECT
+                    count(*) OVER() AS count,
+                    aois.id,
+                    aois.iter_id,
+                    aois.name,
+                    aois.created,
+                    aois.updated
+                FROM
+                    aois
+                WHERE
+                    aois.name ~ ${query.filter}
+                ORDER BY
+                    ${sql.identifier(['aois', query.sort])} ${query.order}
+                LIMIT
+                    ${query.limit}
+                OFFSET
+                    ${query.limit * query.page}
+            `);
+        } catch (err) {
+            throw new Err(500, err, 'Internal AOI Error');
+        }
+
+        return ProjectAOI.deserialize(pgres.rows);
+    }
+
+    serialize() {
+        return {
+            id: this.id,
+            pid: this.pid,
+            iter_id: this.iter_id,
+            created: this.created,
+            updated: this.updated,
+            name: this.name,
+            bounds: this.bounds
+        };
+    }
+
+    async commit(pool) {
+        try {
+            await pool.query(sql`
+                UPDATE aois
+                    SET
+                        name        = ${this.name},
+                        updated     = NOW()
+                    WHERE
+                        id = ${this.id}
+            `);
+
+            return this;
+        } catch (err) {
+            throw new Err(500, err, 'Failed to save AOI');
+        }
+    }
+
+    static async generate(pool, aoi) {
+        try {
+            const pgres = await pool.query(sql`
+                INSERT INTO aois (
+                    pid,
+                    iter_id,
+                    name,
+                    bounds
+                ) VALUES (
+                    ${aois.pid},
+                    ${aois.iter_id},
+                    ${aois.name},
+                    ${aois.bounds}
+                ) RETURNING *
+            `);
+
+            return ProjectAOI.deserialize(pgres.rows[0]);
+        } catch (err) {
+            throw new Err(500, err, 'Failed to generate AOI');
+        }
+    }
+}
+
+module.exports = ProjectAOI;
