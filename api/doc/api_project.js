de475f07fb38cbc80ee4a0e9622630d501774d87
ingalls <nick@ingalls.ca>
2021-08-12 12:11:08 -0600

    Update Task schema for valid APIDoc

--- api/doc/api_project.js
+++ api/doc/api_project.js
@@ -8,7 +8,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2021-08-10T14:04:16.860Z",
+    "time": "2021-08-12T18:10:48.947Z",
     "url": "https://apidocjs.com",
     "version": "0.29.0"
   }
