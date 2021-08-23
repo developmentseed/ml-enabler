eac8fe37381995902d2d18ed9a1acc035c7c482c
ingalls <nick@ingalls.ca>
2021-08-23 11:46:40 -0600

    Add Download Asset schemas

--- api/doc/api_project.js
+++ api/doc/api_project.js
@@ -8,7 +8,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2021-08-12T18:10:48.947Z",
+    "time": "2021-08-23T17:46:28.353Z",
     "url": "https://apidocjs.com",
     "version": "0.29.0"
   }
