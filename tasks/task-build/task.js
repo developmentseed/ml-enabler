30fb7e0777a99cbceb3b06a9b9b0d5b75771a841
ingalls <nick@ingalls.ca>
2021-08-24 09:26:22 -0600

    Add linting & update task-build

--- tasks/task-build/task.js
+++ tasks/task-build/task.js
@@ -1,9 +1,9 @@
 #!/usr/bin/env node
+'use strict';
 
 const unzipper = require('unzipper');
 const find = require('find');
 const request = require('request');
-const Q = require('d3-queue').queue;
 const mkdir = require('mkdirp').sync;
 const pipeline = require('stream').pipeline;
 const fs = require('fs');
@@ -30,7 +30,7 @@ async function main() {
         if (!process.env.AWS_REGION) throw new Error('AWS_REGION env var not set');
         if (!process.env.API_URL) throw new Error('API_URL env var not set');
 
-        const tmp = os.tmpdir() + '/' + Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15)
+        const tmp = os.tmpdir() + '/' + Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
 
         const model = process.env.MODEL;
 
@@ -38,20 +38,20 @@ async function main() {
 
         console.error(process.env);
 
-        const model_id = get_model_id(model)
-        const prediction_id = get_prediction_id(model)
+        const project_id = get_project_id(model);
+        const iteration_id = get_iteration_id(model);
 
         const links = {
             modelLink: model
-        }
+        };
 
         if (process.env.AWS_BATCH_JOB_ID) {
             const logLink = await log_link();
 
-            links.logLink = logLink;
+            links.log_link = logLink;
         }
 
-        await set_link(model_id, prediction_id, links)
+        await set_link(project_id, iteration_id, links);
 
         const dd = await dockerd();
 
@@ -61,26 +61,26 @@ async function main() {
 
         const finalLinks = await docker(tmp, model);
 
-        links.saveLink = finalLinks.save;
-        links.dockerLink = finalLinks.docker;
+        links.save_link = finalLinks.save;
+        links.docker_link = finalLinks.docker;
 
-        await set_link(model_id, prediction_id, links);
+        await set_link(project_id, iteration_id, links);
 
         dd.kill();
-    } catch(err) {
+    } catch (err) {
         console.error(err);
         process.exit(1);
     }
 }
 
-function get_model_id(model) {
-    // ml-enabler-test-1234-us-east-1/models/1/prediction/18/model.zip
-    return parseInt(model.split('/')[2])
+function get_project_id(model) {
+    // ml-enabler-test-1234-us-east-1/project/1/iteration/18/model.zip
+    return parseInt(model.split('/')[2]);
 }
 
-function get_prediction_id(model) {
-    // ml-enabler-test-1234-us-east-1/models/1/prediction/18/model.zip
-    return parseInt(model.split('/')[4])
+function get_iteration_id(model) {
+    // ml-enabler-test-1234-us-east-1/project/1/iteration/18/model.zip
+    return parseInt(model.split('/')[4]);
 }
 
 function log_link() {
@@ -93,7 +93,7 @@ function log_link() {
             console.error(`ok - getting meta for job: ${process.env.AWS_BATCH_JOB_ID}`);
 
             batch.describeJobs({
-                jobs: [ process.env.AWS_BATCH_JOB_ID ]
+                jobs: [process.env.AWS_BATCH_JOB_ID]
             }, (err, res) => {
                 if (err) return reject(err);
 
@@ -106,16 +106,16 @@ function log_link() {
                         return link();
                     }, 10000);
                 } else {
-                    resolve(res.jobs[0].container.logStreamName)
+                    resolve(res.jobs[0].container.logStreamName);
                 }
             });
         }
     });
 }
 
-function set_link(model, prediction, patch) {
+function set_link(project, iteration, patch) {
     return new Promise((resolve, reject) => {
-        console.error(`ok - saving model (${model}), prediction (${prediction}) state: ${JSON.stringify(patch)}`);
+        console.error(`ok - saving project (${project}), iteration (${iteration}) state: ${JSON.stringify(patch)}`);
 
         const url = new URL(process.env.API_URL);
         url.username = 'machine';
@@ -123,7 +123,7 @@ function set_link(model, prediction, patch) {
 
         request({
             method: 'PATCH',
-            url: `${String(url)}/v1/model/${model}/prediction/${prediction}`,
+            url: `${String(url)}/api/project/${project}/iteration/${iteration}`,
             json: true,
             body: patch
         }, (err, res) => {
@@ -153,7 +153,7 @@ function std_model(tmp) {
             }, (err) => {
                 if (err) return reject(err);
 
-                console.error(tmp + '/MODEL/001/')
+                console.error(tmp + '/MODEL/001/');
                 return resolve(tmp + '/MODEL/001/');
             });
         });
@@ -174,13 +174,13 @@ function get_zip(tmp, model) {
             unzipper.Extract({
                 path: path.resolve(tmp, '/src')
             }),
-        (err, res) => {
-            if (err) return reject(err);
+            (err) => {
+                if (err) return reject(err);
 
-            console.error(`ok - saved: ${loc}`);
+                console.error(`ok - saved: ${loc}`);
 
-            return resolve(loc);
-        });
+                return resolve(loc);
+            });
     });
 }
 
@@ -196,7 +196,7 @@ function dockerd() {
             if (/API listen on/.test(data)) {
                 setTimeout(() => {
                     return resolve(dockerd);
-                }, 5000)
+                }, 5000);
             }
         }).on('error', (err) => {
             return reject(err);
@@ -204,85 +204,77 @@ function dockerd() {
     });
 }
 
-function docker(tmp, model) {
-    return new Promise((resolve, reject) => {
-        const tagged_model = model.split('/').splice(1).join('-').replace(/\-model\.zip/, '');
+async function docker(tmp, model) {
+    const tagged_model = model.split('/').splice(1).join('-').replace(/-model\.zip/, '');
 
-        try {
-            console.error('ok - pulling tensorflow/serving docker image');
+    console.error('ok - pulling tensorflow/serving docker image');
 
-            CP.execSync(`
-                docker pull tensorflow/serving:2.1.0-gpu
-            `);
+    CP.execSync(`
+        docker pull tensorflow/serving:2.1.0-gpu
+    `);
 
-            // Ignore errors, these are to ensure the next commands don't err
-            try {
-                CP.execSync(`
-                    docker kill serving_base
-                `);
-            } catch(err) {
-                console.error('ok - no old task to stop');
-            }
+    // Ignore errors, these are to ensure the next commands don't err
+    try {
+        CP.execSync(`
+            docker kill serving_base
+        `);
+    } catch (err) {
+        console.error('ok - no old task to stop');
+    }
 
-            try {
-                CP.execSync(`
-                    docker rm serving_base
-                `);
-            } catch(err) {
-                console.error('ok - no old image to remove');
-            }
+    try {
+        CP.execSync(`
+            docker rm serving_base
+        `);
+    } catch (err) {
+        console.error('ok - no old image to remove');
+    }
 
-            CP.execSync(`
-                docker run -d --name serving_base tensorflow/serving:2.1.0-gpu
-            `);
+    CP.execSync(`
+        docker run -d --name serving_base tensorflow/serving:2.1.0-gpu
+    `);
 
-            CP.execSync(`
-                docker cp ${tmp}/MODEL/ serving_base:/models/default/ \
-            `);
+    CP.execSync(`
+        docker cp ${tmp}/MODEL/ serving_base:/models/default/ \
+    `);
 
-            const tag = `developmentseed/default:${Math.random().toString(36).substring(2, 15)}`;
+    const tag = `developmentseed/default:${Math.random().toString(36).substring(2, 15)}`;
 
-            CP.execSync(`
-                docker commit --change "ENV MODEL_NAME default" serving_base ${tag}
-            `);
+    CP.execSync(`
+        docker commit --change "ENV MODEL_NAME default" serving_base ${tag}
+    `);
 
-            console.error(`ok - docker: ${tag}`);
+    console.error(`ok - docker: ${tag}`);
 
-            const push = `${process.env.AWS_ACCOUNT_ID}.dkr.ecr.${process.env.AWS_REGION}.amazonaws.com/${process.env.BATCH_ECR}:${tagged_model}`;
-            CP.execSync(`
-                docker tag ${tag} ${push}
-            `);
+    const push = `${process.env.AWS_ACCOUNT_ID}.dkr.ecr.${process.env.AWS_REGION}.amazonaws.com/${process.env.BATCH_ECR}:${tagged_model}`;
+    CP.execSync(`
+        docker tag ${tag} ${push}
+    `);
 
-            CP.execSync(`
-                $(aws ecr get-login --region us-east-1 --no-include-email)
-            `)
+    CP.execSync(`
+        $(aws ecr get-login --region us-east-1 --no-include-email)
+    `);
 
-            CP.execSync(`
-                docker push ${push}
-            `);
-            console.error('ok - pushed image to AWS:ECR');
+    CP.execSync(`
+        docker push ${push}
+    `);
+    console.error('ok - pushed image to AWS:ECR');
 
-            CP.execSync(`
-                docker save ${tag} | gzip > ${tmp}/docker-${tagged_model}.tar.gz
-            `)
-            console.error('ok - saved image to disk');
-        } catch(err) {
-            return reject(err);
-        }
+    CP.execSync(`
+        docker save ${tag} | gzip > ${tmp}/docker-${tagged_model}.tar.gz
+    `);
+    console.error('ok - saved image to disk');
 
-        s3.putObject({
-            Bucket: model.split('/')[0],
-            Key: model.split('/').splice(1).join('/').replace(/model\.zip/, `docker-${tagged_model}.tar.gz`),
-            Body: fs.createReadStream(path.resolve(tmp, `docker-${tagged_model}.tar.gz`))
-        }, (err, res) => {
-            if (err) return reject(err);
+    await s3.putObject({
+        Bucket: model.split('/')[0],
+        Key: model.split('/').splice(1).join('/').replace(/model\.zip/, `docker-${tagged_model}.tar.gz`),
+        Body: fs.createReadStream(path.resolve(tmp, `docker-${tagged_model}.tar.gz`))
+    }).promise();
 
-            console.error('ok - saved image to s3');
+    console.error('ok - saved image to s3');
 
-            return resolve({
-                docker: `${process.env.BATCH_ECR}:${tagged_model}`,
-                save: model.split('/')[0] + '/' + model.split('/').splice(1).join('/').replace(/model\.zip/, `docker-${tagged_model}.tar.gz`)
-            });
-        });
-    });
+    return {
+        docker: `${process.env.BATCH_ECR}:${tagged_model}`,
+        save: model.split('/')[0] + '/' + model.split('/').splice(1).join('/').replace(/model\.zip/, `docker-${tagged_model}.tar.gz`)
+    };
 }
