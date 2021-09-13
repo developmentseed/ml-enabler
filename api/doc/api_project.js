636f160d08ab27ff8e4ddb8073af022e288ada7c
ingalls <nick@ingalls.ca>
2021-09-13 11:24:20 -0600

    Update APIDocs

--- api/doc/api_project.js
+++ api/doc/api_project.js
@@ -8,7 +8,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2021-09-10T17:23:51.769Z",
+    "time": "2021-09-13T17:23:59.588Z",
     "url": "https://apidocjs.com",
     "version": "0.29.0"
   }
