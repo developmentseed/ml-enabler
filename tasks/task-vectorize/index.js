f9bfd9f3303e6aadfad4668f6e4e717510110770
ingalls <nick@ingalls.ca>
2022-01-28 08:31:37 -0700

    Framework test fixtures

--- tasks/task-vectorize/index.js
+++ tasks/task-vectorize/index.js
@@ -15,40 +15,57 @@ const ITERATION = process.env.ITERATION_ID;
 const SUBMISSION = process.env.SUBMISSION_ID;
 const ASSET_BUCKET = process.env.ASSET_BUCKET;
 
-main();
+/**
+ * @class
+ */
+class Task {
 
-async function main() {
-    await tippecanoe.tile(
-        fs.createReadStream(path.resolve(__dirname, './data/input.geojson')),
-        path.resolve(__dirname, './data/fabric.mbtiles'),
-        {
-            layer: 'data',
-            std: true,
-            force: true,
-            name: `Project ${PROJECT} - Iteration ${ITERATION} - Submission ${SUBMISSION}`,
-            attribution: `Project ${PROJECT} - Iteration ${ITERATION} - Submission ${SUBMISSION}`,
-            description: `Project ${PROJECT} - Iteration ${ITERATION} - Submission ${SUBMISSION}`,
-            limit: {
-                features: false,
-                size: false
-            },
-            zoom: {
-                max: 15,
-                min: 10
+    /**
+     * Take an input file and output a TileBase file
+     *
+     * @param {String} input GeoJSON input path
+     * @param {Object} opts Options Object
+     */
+    static async vectorize(input, opts) {
+        await tippecanoe.tile(
+            fs.createReadStream(input),
+            path.resolve(__dirname, './data/fabric.mbtiles'),
+            {
+                layer: 'data',
+                std: true,
+                force: true,
+                name: `Project ${PROJECT} - Iteration ${ITERATION} - Submission ${SUBMISSION}`,
+                attribution: `Project ${PROJECT} - Iteration ${ITERATION} - Submission ${SUBMISSION}`,
+                description: `Project ${PROJECT} - Iteration ${ITERATION} - Submission ${SUBMISSION}`,
+                limit: {
+                    features: false,
+                    size: false
+                },
+                zoom: {
+                    max: 15,
+                    min: 10
+                }
             }
-        }
-    );
+        );
 
-    await TileBase.to_tb(
-        path.resolve(__dirname, './data/fabric.mbtiles'),
-        path.resolve(__dirname, './data/fabric.tilebase')
-    );
+        await TileBase.to_tb(
+            path.resolve(__dirname, './data/fabric.mbtiles'),
+            path.resolve(__dirname, './data/fabric.tilebase')
+        );
 
-    await s3.putObject({
-        ContentType: 'application/octet-stream',
-        Bucket: ASSET_BUCKET,
-        Key: `project/${PROJECT}/iteration/${ITERATION}/submission-${SUBMISSION}.tilebase`,
-        Body: fs.createReadStream(path.resolve(__dirname, './data/fabric.tilebase'))
-    }).promise();
-
+        await s3.putObject({
+            ContentType: 'application/octet-stream',
+            Bucket: ASSET_BUCKET,
+            Key: `project/${PROJECT}/iteration/${ITERATION}/submission-${SUBMISSION}.tilebase`,
+            Body: fs.createReadStream(path.resolve(__dirname, './data/fabric.tilebase'))
+        }).promise();
+    }
 }
+
+if (require.main === module) {
+    Task.vectorize(path.resolve(__dirname, './data/input.geojson'), {
+
+    });
+}
+
+module.exports = Task;
---
+++ tasks/task-vectorize/test/vectorize.test.js
@@ -0,0 +1,8 @@
+const test = require('tape');
+const Task = require('..');
+
+test('vector', async (t) => {
+    Task.vectorize()
+
+    t.end();
+});
