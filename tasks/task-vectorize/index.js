2fb4b3bdef3fd0e24a406cd9861dbfb16e8167a8
ingalls <nick@ingalls.ca>
2022-01-28 08:57:28 -0700

    Add lints & working vectorize test

--- tasks/task-vectorize/index.js
+++ tasks/task-vectorize/index.js
@@ -1,19 +1,11 @@
+'use strict';
 const AWS = require('aws-sdk');
 const path = require('path');
 const fs = require('fs');
 const TileBase = require('tilebase');
 const Tippecanoe = require('./lib/tippecanoe');
 
-const s3 = new AWS.S3({
-    region: process.env.AWS_DEFAULT_REGION || 'us-east-1'
-});
-
 const tippecanoe = new Tippecanoe();
-
-const PROJECT = process.env.PROJECT_ID;
-const ITERATION = process.env.ITERATION_ID;
-const SUBMISSION = process.env.SUBMISSION_ID;
-const ASSET_BUCKET = process.env.ASSET_BUCKET;
 
 /**
  * @class
@@ -25,18 +17,34 @@ class Task {
      *
      * @param {String} input GeoJSON input path
      * @param {Object} opts Options Object
+     * @param {object} [opts.tmp=./data]    Temp Folder
+     * @param {string} opts.project         MLEnabler Project ID
+     * @param {string} opts.iteration       MLEnabler Iteration ID
+     * @param {string} opts.submission      MLEnabler Submission ID
+     * @param {string} [opts.bucket=false]  S3 Bucket to put data - if omitted, data is not uploaded
+     * @param {boolean} [opts.silent=false] Should output be squelched
      */
-    static async vectorize(input, opts) {
+    static async vectorize(input, opts = {}) {
+        if (!opts.tmp) opts.tmp = path.resolve(__dirname, './data/');
+        if (!opts.silent) opts.silent = false;
+        if (!opts.bucket) {
+            opts.bucket = false;
+        } else if (opts.bucket && (!opts.project || !opts.iteration || !opts.submission)) {
+            throw new Error('If opts.bucket is set, opts.<project, iteration, submission> must be also');
+        }
+
+        if (!opts.silent) console.error(`ok - writing to: ${opts.tmp}`);
+
         await tippecanoe.tile(
             fs.createReadStream(input),
-            path.resolve(__dirname, './data/fabric.mbtiles'),
+            path.resolve(opts.tmp, 'fabric.mbtiles'),
             {
                 layer: 'data',
-                std: true,
+                std: !!opts.silent,
                 force: true,
-                name: `Project ${PROJECT} - Iteration ${ITERATION} - Submission ${SUBMISSION}`,
-                attribution: `Project ${PROJECT} - Iteration ${ITERATION} - Submission ${SUBMISSION}`,
-                description: `Project ${PROJECT} - Iteration ${ITERATION} - Submission ${SUBMISSION}`,
+                name: `Project ${opts.project} - Iteration ${opts.iteration} - Submission ${opts.submission}`,
+                attribution: `Project ${opts.project} - Iteration ${opts.iteration} - Submission ${opts.submission}`,
+                description: `Project ${opts.project} - Iteration ${opts.iteration} - Submission ${opts.submission}`,
                 limit: {
                     features: false,
                     size: false
@@ -49,22 +57,37 @@ class Task {
         );
 
         await TileBase.to_tb(
-            path.resolve(__dirname, './data/fabric.mbtiles'),
-            path.resolve(__dirname, './data/fabric.tilebase')
+            path.resolve(opts.tmp, 'fabric.mbtiles'),
+            path.resolve(opts.tmp, 'fabric.tilebase')
         );
 
-        await s3.putObject({
-            ContentType: 'application/octet-stream',
-            Bucket: ASSET_BUCKET,
-            Key: `project/${PROJECT}/iteration/${ITERATION}/submission-${SUBMISSION}.tilebase`,
-            Body: fs.createReadStream(path.resolve(__dirname, './data/fabric.tilebase'))
-        }).promise();
+        if (opts.bucket) {
+            const s3 = new AWS.S3({
+                region: process.env.AWS_DEFAULT_REGION || 'us-east-1'
+            });
+
+            await s3.putObject({
+                ContentType: 'application/octet-stream',
+                Bucket: opts.bucket,
+                Key: `project/${opts.project}/iteration/${opts.iteration}/submission-${opts.submission}.tilebase`,
+                Body: fs.createReadStream(path.resolve(opts.tmp, 'fabric.tilebase'))
+            }).promise();
+        }
     }
 }
 
 if (require.main === module) {
-    Task.vectorize(path.resolve(__dirname, './data/input.geojson'), {
+    const PROJECT = process.env.PROJECT_ID;
+    const ITERATION = process.env.ITERATION_ID;
+    const SUBMISSION = process.env.SUBMISSION_ID;
+    const ASSET_BUCKET = process.env.ASSET_BUCKET;
 
+    Task.vectorize(path.resolve(__dirname, './data/input.geojson'), {
+        silent: false,
+        bucket: ASSET_BUCKET,
+        project: PROJECT,
+        iteration: ITERATION,
+        submission: SUBMISSION
     });
 }
 
