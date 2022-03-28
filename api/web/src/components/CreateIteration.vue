6f81d8997aff03bcb0ee9c970c5fe0622db8d8c5
ingalls <nick@ingalls.ca>
2022-03-28 09:44:44 -0600

    Prepopulate Create Iteration with previous values

--- api/web/src/components/CreateIteration.vue
+++ api/web/src/components/CreateIteration.vue
@@ -8,58 +8,72 @@
             </button>
         </div>
         <div class='border border--gray-light round col col--12 px12 py12 clearfix'>
-            <div class='grid grid--gut12'>
-                <div class='col col--4 py6'>
-                    <InfVersion
-                        @version='iter.version = $event'
-                    />
-                </div>
+            <template v-if='loading.latest'>
+                <Loading desc='Loading Initial Values'/>
+            </template>
+            <template v-else>
+                <div class='grid grid--gut12'>
+                    <div class='col col--4 py6'>
+                        <InfVersion
+                            :_version='iter.version'
+                            @version='iter.version = $event'
+                        />
+                    </div>
 
-                <div class='col col--4 py6'>
-                    <InfModel
-                        @model='iter.model_type = $event'
-                    />
-                </div>
+                    <div class='col col--4 py6'>
+                        <InfModel
+                            :_model_type='iter.model_type'
+                            @model='iter.model_type = $event'
+                        />
+                    </div>
 
-                <div class='col col--4 py6'>
-                    <InfType
-                        @type='iter.inf_type = $event'
-                    />
-                </div>
+                    <div class='col col--4 py6'>
+                        <InfType
+                            :_type='iter.inf_type'
+                            @type='iter.inf_type = $event'
+                        />
+                    </div>
 
-                <div v-if='iter.inf_type !== "detection"' class='col col--12'>
-                    <InfList
-                        @binary='iter.inf_binary = $event'
-                        @list='iter.inf_list = $event'
-                    />
-                </div>
+                    <div v-if='iter.inf_type !== "detection"' class='col col--12'>
+                        <InfList
+                            :_binary='iter.inf_binary'
+                            :_list='iter.inf_list'
+                            @binary='iter.inf_binary = $event'
+                            @list='iter.inf_list = $event'
+                        />
+                    </div>
 
-                <div class='col col--12 pt6'>
-                    <InfImagery
-                        @supertile='iter.inf_supertile = $event'
-                        @zoom='iter.tile_zoom = $event'
-                        @imagery='iter.imagery_id = $event'
-                        @err='$emit("err", $event)'
-                    />
-                </div>
+                    <div class='col col--12 pt6'>
+                        <InfImagery
+                            :_superfile='iter.inf_supertile'
+                            :_zoom='iter.tile_zoom'
+                            :_imagery='iter.imagery_id'
+                            @supertile='iter.inf_supertile = $event'
+                            @zoom='iter.tile_zoom = $event'
+                            @imagery='iter.imagery_id = $event'
+                            @err='$emit("err", $event)'
+                        />
+                    </div>
 
-                <div class='col col--12 pt6'>
-                    <InfGitSha
-                        @gitsha='iter.gitsha = $event'
-                        @err='$emit("err", $event)'
-                    />
-                </div>
+                    <div class='col col--12 pt6'>
+                        <InfGitSha
+                            @gitsha='iter.gitsha = $event'
+                            @err='$emit("err", $event)'
+                        />
+                    </div>
 
-                <div class='col col--12 py12'>
-                    <button @click='postIteration' class='btn btn--stroke round fr color-gray color-green-on-hover'>Add <span v-text='type'/></button>
+                    <div class='col col--12 py12'>
+                        <button @click='postIteration' class='btn btn--stroke round fr color-gray color-green-on-hover'>Add <span v-text='type'/></button>
+                    </div>
                 </div>
-            </div>
+            </template>
         </div>
     </div>
 
 </template>
 
 <script>
+import Loading from './util/Loading.vue';
 import InfType from './util/InfType.vue';
 import InfList from './util/InfList.vue';
 import InfVersion from './util/InfVersion.vue';
@@ -73,7 +87,7 @@ export default {
         return {
             type: this.$route.name === 'createTraining' ? 'Training' : 'Iteration',
             loading: {
-                imagery: true
+                latest: true
             },
             imagery: [],
             iter: {
@@ -89,7 +103,28 @@ export default {
             }
         };
     },
+    mounted: async function() {
+        await this.fetchLatest();
+    },
     methods: {
+        fetchLatest: async function() {
+            try {
+                const latest = await window.std(`/api/project/${this.$route.params.projectid}/iteration/latest`);
+
+                this.iter.version = latest.version;
+                this.iter.imagery_id = latest.imagery_id;
+                this.iter.tile_zoom = latest.tile_zoom;
+                this.iter.inf_list = latest.inf_list;
+                this.iter.model_type = latest.model_type;
+                this.iter.inf_type = latest.inf_type;
+                this.iter.inf_supertile = latest.inf_supertile;
+            } catch (err) {
+                // Errors are ignored here as errors don't stop the user from creating an iteration
+                console.error(err);
+            }
+
+            this.loading.latest = false;
+        },
         postIteration: async function() {
             if (!/^\d+\.\d+\.\d+$/.test(this.iter.version)) {
                 return this.$emit('err', new Error('Version must be valid semver'));
@@ -124,6 +159,7 @@ export default {
         }
     },
     components: {
+        Loading,
         InfType,
         InfList,
         InfVersion,
